1c565d0dfc66666decf5ca469457f8a334c12291
Michał Pasynek <michalpasynek@gmail.com>
2022-07-21 14:54:48 +0200

    Add main function and DOM elements

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ let todoInput //todo info
 let errorInfo //info about no exist todo position
 let addBtn //button ADD
 let ulList //todo lists
+
+const main = () => {
+	prepareDOMElements()
+	prepareDOMEvents()
+}
+
+const prepareDOMElements = () => {
+    todoInput = document.querySelector('.todo-input')
+    errorInfo = document.querySelector('.error-info')
+    addBtn = document.querySelector('.btn-add')
+    ulList = document.querySelector('.todoList ul')
+}
+
+const prepareDOMEvents = () => {}
+
+document.addEventListener('DOMContentLoaded', main)
