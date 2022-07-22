5d91ebafe0cc4af83da0a4942436d167c498232a
Michał Pasynek <michalpasynek@gmail.com>
2022-07-22 14:17:41 +0200

    Add todos position and tools to manage it

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let todoInput //todo info
 let errorInfo //info about no exist todo position
 let addBtn //button ADD
 let ulList //todo lists
+let newTodo //latest add todos
 
 const main = () => {
 	prepareDOMElements()
@@ -9,12 +10,47 @@ const main = () => {
 }
 
 const prepareDOMElements = () => {
-    todoInput = document.querySelector('.todo-input')
-    errorInfo = document.querySelector('.error-info')
-    addBtn = document.querySelector('.btn-add')
-    ulList = document.querySelector('.todoList ul')
+	todoInput = document.querySelector('.todo-input')
+	errorInfo = document.querySelector('.error-info')
+	addBtn = document.querySelector('.btn-add')
+	ulList = document.querySelector('.todolist ul')
 }
 
-const prepareDOMEvents = () => {}
+const prepareDOMEvents = () => {
+	addBtn.addEventListener('click', addNewTodo)
+}
+
+const addNewTodo = () => {
+	if (todoInput.value !== '') {
+		newTodo = document.createElement('li')
+		newTodo.textContent = todoInput.value
+		createToolsArea()
+		ulList.append(newTodo)
+		todoInput.value = ''
+		errorInfo.textContent = ''
+	} else {
+		errorInfo.textContent = 'Wpisz treść zadania!'
+	}
+}
+
+const createToolsArea = () => {
+	const toolsPanel = document.createElement('div')
+	toolsPanel.classList.add('tools')
+	newTodo.append(toolsPanel)
+
+	const completeBtn = document.createElement('button')
+	completeBtn.classList.add('complete')
+	completeBtn.innerHTML = '<i class="fas fa-check"></i>'
+
+	const editBtn = document.createElement('button')
+	editBtn.classList.add('edit')
+	editBtn.textContent = 'EDIT'
+
+	const deleteBtn = document.createElement('button')
+	deleteBtn.classList.add('delete')
+	deleteBtn.innerHTML = '<i class="fas fa-times"></i>'
+
+	toolsPanel.append(completeBtn, editBtn, deleteBtn)
+}
 
 document.addEventListener('DOMContentLoaded', main)
