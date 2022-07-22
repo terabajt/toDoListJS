52368e862c18a83728caef91d09b22eb53008741
Michał Pasynek <michalpasynek@gmail.com>
2022-07-22 14:32:31 +0200

    Add listening for a buttons and add completed button

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ const prepareDOMElements = () => {
 
 const prepareDOMEvents = () => {
 	addBtn.addEventListener('click', addNewTodo)
+	ulList.addEventListener('click', checkClick)
 }
 
 const addNewTodo = () => {
@@ -51,6 +52,17 @@ const createToolsArea = () => {
 	deleteBtn.innerHTML = '<i class="fas fa-times"></i>'
 
 	toolsPanel.append(completeBtn, editBtn, deleteBtn)
+}
+
+const checkClick = e => {
+	if (e.target.matches('.complete')) {
+		e.target.closest('li').classList.toggle('completed')
+		e.target.classList.toggle('completed')
+	} else if (e.target.matches('.edit')) {
+		console.log('edit')
+	} else if (e.target.matches('.delete')) {
+		console.log('delete')
+	}
 }
 
 document.addEventListener('DOMContentLoaded', main)
